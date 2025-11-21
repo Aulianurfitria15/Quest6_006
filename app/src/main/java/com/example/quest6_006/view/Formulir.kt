f2465925fbd0f3de6c63fbe8a27251856a985e9f
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-11-21 15:45:07 +0700

    menyesuaikan outlined text field txt alamat dan button click

--- app/src/main/java/com/example/quest6_006/view/Formulir.kt
+++ app/src/main/java/com/example/quest6_006/view/Formulir.kt
@@ -94,3 +94,32 @@ fun FormSiswa(
                     }
                 }
             }
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .width(250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
+            OutlinedTextField(
+                value = txtAlamat,
+                singleLine = true,
+                modifier = Modifier
+                    .width(250.dp),
+                label = { Text(text = "Alamat") },
+                onValueChange = {
+                    txtAlamat = it
+                },
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(1f)
+                    .padding(all = 25.dp),
+                enabled = txtAlamat.isNotEmpty(),
+                onClick = {onSubmitButtonClicked(listData)}
+            ) {
+                Text(stringResource(id = R.string.submit))
+            }
+        }
+    }
+}
