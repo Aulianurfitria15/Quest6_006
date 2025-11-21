ba444adcb07f3ae47bdc8120336452127bcfcefb
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-11-21 16:04:03 +0700

    menambahkan parameter baru di function tampilansiswa

--- app/src/main/java/com/example/quest6_006/view/TampilanData.kt
+++ app/src/main/java/com/example/quest6_006/view/TampilanData.kt
@@ -24,42 +24,11 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.quest6_006.R
+import com.example.quest6_006.model.Siswa
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TampilanData(
-    onBackBtnClick: () -> Unit
+fun TampilanSiswa(
+    statusUiSiswa: Siswa,
+    onBackButtonClicked: () -> Unit
 ){
-    val items = listOf(
-        Pair(stringResource(id = R.string.nama_lengkap), "Aul"),
-        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
-        Pair(stringResource(id = R.string.alamat), "Yogyakarta")
-    )
-    Scaffold (modifier = Modifier,
-        {
-            TopAppBar(
-                title = { Text(text = stringResource(id = R.string.tampil), color = Color.White) },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.teal_700))
-            )
-        }){ isiRuang ->
-        Column (modifier = Modifier.padding(isiRuang),
-            verticalArrangement = Arrangement.SpaceBetween){
-            Column (modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
-                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))) {
-                items.forEach { item ->
-                    Column {
-                        Text(text = item.first.uppercase(), fontSize = 16.sp)
-                        Text(text = item.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive, fontSize = 22.sp)
-                    }
-                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
-                }
-                Spacer(modifier = Modifier.height(10.dp))
-                Button(
-                    modifier = Modifier.fillMaxWidth(),
-                    onClick = onBackBtnClick) {
-                    Text(text = stringResource(id = R.string.back))
-                }
-            }
-        }
-    }
-}
