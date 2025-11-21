c960b82d0320b0c6ae3612d901154fb8723cc269
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-11-21 15:43:00 +0700

    manambahkan paramter dan jga variabel

--- app/src/main/java/com/example/quest6_006/view/Formulir.kt
+++ app/src/main/java/com/example/quest6_006/view/Formulir.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +21,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,72 +36,13 @@ import com.example.quest6_006.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FormIsian(
-    jenisK: List<String> = listOf("Laki-Laki", "Perempuan"),
-    OnSubmitBtnClick : () -> Unit
+fun FormSiswa(
+    pilihanJK: List<String>,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
 ){
-    Scaffold (modifier = Modifier,
-        {
-            TopAppBar(
-                title = { Text(text = stringResource(id= R.string.home), color = Color.White)},
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    colorResource(id = R.color.teal_700))
-            )}
-    ) { isiRuang ->
-        Column(
-            modifier = Modifier.padding(isiRuang),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            OutlinedTextField(
-                value = "",
-                singleLine = true,
-                modifier = Modifier
-                    .padding(top = 20.dp)
-                    .width(width = 250.dp),
-                label = { Text(text = "Nama Lengkap") },
-                onValueChange = {}
-            )
-            HorizontalDivider(
-                modifier = Modifier
-                    .padding(20.dp)
-                    .width(250.dp), thickness = Thickness, color = Color.Red
-            )
-            Row {
-                jenisK.forEach { item ->
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        RadioButton(
-                            selected = false,
-                            onClick = { item }
-                        )
-                        Text(text = item)
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    var txtAlamat by remember { mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("") }
+    val listData: MutableList<String> = mutableListOf(txtNama, txtGender, txtAlamat)
 
-                    }
-                }
-            }
-            HorizontalDivider(
-                modifier = Modifier
-                    .padding(20.dp)
-                    .width(250.dp),
-                thickness = 1.dp,
-                color = Color.Red
-            )
-            OutlinedTextField(
-                value = "",
-                singleLine = true,
-                modifier = Modifier
-                    .width(250.dp),
-                label = { Text(text = "Alamat") },
-                onValueChange = {},
-            )
-            Spacer(modifier = Modifier.height(30.dp))
-            Button(
-                modifier = Modifier.fillMaxWidth(1f)
-                    .padding(all = 25.dp),
-                onClick = OnSubmitBtnClick
-            ) {
-                Text(stringResource(id = R.string.submit))
-            }
-        }
-    }
-}
